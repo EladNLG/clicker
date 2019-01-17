8af734131296972b929028c9d0cc63609201d5e2
Elad <e1lad8955@gmail.com>
2019-01-17 17:45:23 +0200

    hi

---
+++ index.js
@@ -0,0 +1,15 @@
+var click = 1
+var cost = click * 10
+function add1(){
+    document.getElementById("counter").innerHTML = document.getElementById("counter").innerHTML * 1 + click
+}
+function more(){
+    var amount = document.getElementById("counter").innerHTML * 1
+    if (amount >= cost){
+        document.getElementById("counter").innerHTML = document.getElementById("counter").innerHTML * 1 - cost;
+        click += Math.floor(Math.random() * 5)
+        cost = click * 10 + Math.floor(Math.random() * 25)
+        document.getElementById("cost").innerHTML = "Cost: " + cost + "$"
+        document.getElementById("clickW").innerHTML = "Click: " + click + "$"
+    }
+}

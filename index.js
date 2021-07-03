4a042692acfbbb8f579ff681ae1d4adf5c5f1c69
EladNLG <44613424+EladNLG@users.noreply.github.com>
2021-07-03 18:34:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var click = 1
 var cost = click * 10
-var amount = 100000000000000000;
+var amount = 1000000000;
 function add1(){
     document.getElementById("counter").innerHTML = amount + click
     amount += click

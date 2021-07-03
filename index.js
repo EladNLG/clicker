c13e63bc415a52253241b0b3c026bb76ac1a01de
EladNLG <44613424+EladNLG@users.noreply.github.com>
2021-07-03 18:33:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var click = 1
 var cost = click * 10
+var amount = 100000000000000000;
 function add1(){
-    document.getElementById("counter").innerHTML = document.getElementById("counter").innerHTML * 1 + click
+    document.getElementById("counter").innerHTML = amount + click
+    amount += click
 }
 function more(){
-    var amount = document.getElementById("counter").innerHTML * 1
     if (amount >= cost){
         document.getElementById("counter").innerHTML = document.getElementById("counter").innerHTML * 1 - cost;
         click += Math.floor(Math.random() * 4) + 1
